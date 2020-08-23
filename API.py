7ea405424100bacb5a01c8bd784d5e91e63123e4
MJO-KnekY <mathieu.josset@gmail.com>
2020-08-23 17:48:55 +0200

    Update API.py

--- API.py
+++ API.py
@@ -8,31 +8,40 @@ sess = tf.compat.v1.Session(config=config)
 
 import time
 
+from tensorflow.keras.applications import InceptionResNetV2
+from tensorflow.keras.layers import Input
+
 import keras
 
 import streamlit as st
 
 from PIL import Image
 
-from tensorflow.keras.models import Model, Sequential, load_model
+import re
+
+from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
+
+from keras.applications.imagenet_utils import decode_predictions
 
 import numpy as np
 import pandas as pd
+
+from keras import backend as K
+import keras.backend.tensorflow_backend as tfback
 
 st.set_option('deprecation.showfileUploaderEncoding', False)
 
 @st.cache(persist=True)
 def predict(img):
-    #model = load_model('model')
-    model = load_model('s3://mjo-dogs-breeds-mdl')
-    labels = np.array(pd.read_csv('idx_to_class.csv')['0'])
-    img = load_img('tst.jpg',target_size=(224,224))
+    model = InceptionResNetV2(weights="imagenet",
+                  include_top=True,
+                  input_tensor=Input(shape=(224, 224, 3)))
     img = img_to_array(img)
     img = img.reshape((-1, img.shape[0], img.shape[1], 3)) / 255
     rez = model.predict(img)
-    rez = pd.Series(rez[0][np.argpartition(rez,-3)[0][-3:]],labels[np.argpartition(rez,-3)[0][-3:]])
-    rez = rez[rez>0.1]*100
+    rez = pd.DataFrame(decode_predictions(rez)[0],columns=['_','breed','prct']).drop(columns='_').sort_values(by='prct', ascending=False)
+    np.array(rez)
     return rez
     
 st.title("Dog Breed Classifier\nCNN ResNetV2")
@@ -46,5 +55,6 @@ if uploaded_file is not None:
     with st.spinner("...Sorting results..."):
         time.sleep(5)
         rez = predict(image)
-        for i in range(rez.size):
-            st.write('I am %.2f% sure that this is a %s.' % (rez[i], rez.index[i]))
+        for i in range(len(rez)):
+            if rez[i][1]>0.1:
+                st.write('I am %.2f% sure that this is a %s.' % (rez[i][1]*100, re.sub('_', ' ',rez[i][0])))
